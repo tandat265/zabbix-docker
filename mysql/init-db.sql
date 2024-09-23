3bd4b3b70c3d3421523f69b7b1247eb146abd0ef
Ngo Tan Dat <tandat26502@gmail.com>
2024-09-23 09:52:31 -0400

    fix docker-compose

--- mysql/init-db.sql
+++ mysql/init-db.sql
@@ -1,8 +1,8 @@
--- Tạo cơ sở dữ liệu Zabbix
-CREATE DATABASE zabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
+-- Tạo cơ sở dữ liệu Zabbix nếu chưa tồn tại
+CREATE DATABASE IF NOT EXISTS zabbix CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
--- Tạo người dùng Zabbix
-CREATE USER 'zabbix'@'localhost' IDENTIFIED BY 'zabbix';
+-- Tạo người dùng Zabbix nếu chưa tồn tại
+CREATE USER IF NOT EXISTS 'zabbix'@'localhost' IDENTIFIED BY 'zabbix';
 
 -- Gán quyền cho người dùng Zabbix
 GRANT ALL PRIVILEGES ON zabbix.* TO 'zabbix'@'localhost';
