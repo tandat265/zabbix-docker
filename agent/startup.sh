d6f9fcdc7f1e09832364ae2572ccede398397e64
Ngo Tan Dat <tandat26502@gmail.com>
2024-10-08 10:50:37 -0400

    final part 5

--- agent/startup.sh
+++ agent/startup.sh
@@ -3,8 +3,12 @@ service nginx start
 service elasticsearch start
 service ssh start
 service zabbix-agent start
+service rsyslog start
 filebeat -e -c /etc/filebeat/filebeat.yml
 chown -R www-data:www-data /var/log/nginx/ 
+touch /var/log/auth.log
+chmod 644 /var/log/auth.log
+chown syslog:adm /var/log/auth.log
 chmod 644 /var/log/nginx/access.log
 chmod 644 /var/log/nginx/error.log
 python3.10 /ai/kafka_consumer.py
